27508d29a332086ca49b53d6e6cd5d69b9d7e137
alramosf <106263609+alramosf@users.noreply.github.com>
2022-11-12 15:01:42 -0300

    calculadora de juros

--- exercicios/ex003.js
+++ exercicios/ex003.js
@@ -7,21 +7,6 @@ a vista no dinheiro ou pix, 15% de desconto
 em duas vezes, preço normal sem juros
 acima de duas vezes, preço normal mais 10% de juros*/
 
-let price = 875 ;
-let debtDiscount = 0.1
-let moneyPix = 0.15
-const valueNoDiscount = 0
-let valueCalcDebt = price * debtDiscount
-let valueCalcMoneyPix = price * moneyPix
-let valueCalc
-let valueWithFees = -0.1
-
-console.log(price - valueCalcDebt)
-
-
-
-
-
 
 //var salario = 1000;
 //var percentual = 0.07;
@@ -29,3 +14,32 @@ console.log(price - valueCalcDebt)
 //var novo_salario = salario + aumento;
 
 //alert(novo_salario)
+
+let price = 875 ;
+let debtDiscount = 0.1
+let moneyPix = 0.15
+let valueWithFees = -0.1
+const valueNoDiscount = price
+
+let valueCalcDebt = price * debtDiscount 
+let valueCalcMoneyPix = price * moneyPix
+let valueCalcNoDiscount = price
+let valueCalcWithFees = price * valueWithFees
+
+let paymentMethod = 'other'
+//let finalPrice = ''
+//console.log(price - valueCalcWithFees)
+
+if(paymentMethod === 'debit' ){
+    finalPrice = price - valueCalcDebt
+    console.log(finalPrice)
+} else if(paymentMethod === 'pix'){
+    finalPrice = price - valueCalcMoneyPix
+    console.log (finalPrice)
+} else if (paymentMethod === 'twotimes') {
+    finalPrice = valueCalcNoDiscount
+    console.log (finalPrice)
+} else{
+    finalPrice =  price - valueCalcWithFees
+    console.log (finalPrice)
+}
