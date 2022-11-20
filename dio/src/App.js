f5165fce17d926a23fc47e54f3472c7f77ab379d
alramosf <106263609+alramosf@users.noreply.github.com>
2022-11-20 18:39:50 -0300

    react

---
+++ dio/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <Button title="Entrar"/>
+      <Button title="Salvar"/>
+    </div>
+  );
+}
+export default App;
